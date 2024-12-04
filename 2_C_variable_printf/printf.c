5ab95c32e432931f0b3c05e7eb71574e534db0f9
vaishali <vaishalichauhan2801@gmail.com>
2024-12-04 10:38:10 +0530

    Add files via upload
    upload file

---
+++ 2_C_variable_printf/printf.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+ int main(){
+ 	
+ 	int a = 34;
+ 	int b = 56;
+ 	int c = a+b;
+ 	int d = a-b;
+ 	int e = a*b;
+ 	int f = a%b;
+ 	
+ 	printf("addition : %d \n",c);
+ 	printf("subtraction: %d \n",d);
+ 	printf("multiply : %d \n",e);
+ 	printf("modul: %d \n",f);
+ 	
+ 	
+ }
